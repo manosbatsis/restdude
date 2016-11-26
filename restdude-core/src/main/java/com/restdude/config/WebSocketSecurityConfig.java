2d37c1673a6328edcb45e4136ce88e059fe3a85d
manos <manos@sobol>
2016-11-26 12:22:22 +0200

    added generic configurable configs

---
+++ restdude-core/src/main/java/com/restdude/config/WebSocketSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.restdude.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages
+                .simpMessageDestMatchers("/queue/**", "/topic/**").denyAll()
+                .simpSubscribeDestMatchers("/queue/**/*-user*", "/topic/**/*-user*").denyAll()
+                .anyMessage().authenticated();
+
+    }
+
+    /**
+     * Disables CSRF for Websockets.
+     */
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+}
