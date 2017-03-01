f0e1cb6bd1904239d71f291f40a7cdf0491ece2f
delta750 <nkapetanas@hotmail.com>
2017-03-01 15:38:22 +0200

    fixed jshint error and ember test errors

--- restdude-client-ember/app/components/login-form.js
+++ restdude-client-ember/app/components/login-form.js
@@ -1,9 +1,10 @@
 // app/components/login-form.js
 import Ember from 'ember';
 export default Ember.Component.extend({
+     //session: Ember.inject.service(),
     authenticator: 'authenticator:oauth2',
     actions: {
-        session: service('session'),
+        session: Ember.inject.service('session'),
         authenticate: function() {
             var credentials = this.getProperties('identification', 'password');
             this.get('session').authenticate('authenticator:oauth2', credentials).catch((message) => {
