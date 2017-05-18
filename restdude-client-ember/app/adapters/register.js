3280b84ffa897b7656c9bfe4c64736c017cc357b
delta750 <nkapetanas@hotmail.com>
2017-05-18 20:24:29 +0300

    deleted register adapter

--- restdude-client-ember/app/adapters/register.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
-
-export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-  //namespace: 'restdude/api/rest',
-  host: 'http://localhost:8080/restdude/api/rest',
-  namespace: 'api/auth/account',
-  authorizer: 'authorizer:token'
-});
