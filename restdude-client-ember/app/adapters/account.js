63fdf606b4faa20760c8cb40385b067db6a30a3a
delta750 <nkapetanas@hotmail.com>
2017-05-19 22:17:38 +0300

    added user name on nav bar

--- restdude-client-ember/app/adapters/account.js
+++ restdude-client-ember/app/adapters/account.js
@@ -3,4 +3,14 @@ import config from "../config/environment";
 
 export default ApplicationAdapter.extend({
   namespace: `${config.namespaceAuth}`,
+  
+  urlForQueryRecord(query) {
+    if (query.me) {
+      delete query.me;
+      return `${this._super(...arguments)}/me`;
+    }
+
+    return this._super(...arguments);
+  }
+
 });
--- restdude-client-ember/app/adapters/user.js
+++ restdude-client-ember/app/adapters/user.js
@@ -3,5 +3,12 @@
 import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
+  urlForQueryRecord(query) {
+    if (query.me) {
+      delete query.me;
+      return `${this._super(...arguments)}/me`;
+    }
 
+    return this._super(...arguments);
+  }
 });
