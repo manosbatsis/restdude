0a5e4bd26ac8762f6976a5d8cc3cc4045fe517b3
delta750 <nkapetanas@hotmail.com>
2017-05-27 22:33:05 +0300

    fixed commit

---
+++ restdude-client-ember/app/routes/spaces/show.js
@@ -0,0 +1,5 @@
+import BaseAuthenticated  from '..base-authenticated';
+import SaveModelMixin from 'super-rentalsmixinsspacessave-model-mixin';
+
+export default BaseAuthenticated.extend(SaveModelMixin, {
+});
