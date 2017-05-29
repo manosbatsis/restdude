364f6a217a0070142683bc3c09b39345d9e77170
manos <manos@sobol>
2017-05-30 02:58:17 +0300

    fixed members rendering

--- restdude-client-ember/app/models/context-membership.js
+++ restdude-client-ember/app/models/context-membership.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   admin: DS.attr('boolean'),
-  user: DS.belongsTo('user'),
+  user: DS.attr(),//DS.internalModel('user'),
   space: DS.belongsTo('space'),
 });
--- restdude-client-ember/app/serializers/application.js
+++ restdude-client-ember/app/serializers/application.js
@@ -1,9 +1,10 @@
 // app/serializers/application.js
 import Ember from 'ember';
 import DS from 'ember-data';
+
 export default DS.JSONAPISerializer.extend({
     keyForAttribute(key) { return key; },
-    keyForRelationship: function(key) {
+    /*keyForRelationship: function(key) {
         return Ember.String.decamelize(key);
-    }
+    }*/
 });
