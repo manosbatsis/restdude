7b7f785337e60f1952f8ea5c50c8b47584520544
delta750 <nkapetanas@hotmail.com>
2017-05-30 04:20:29 +0300

    updated mebership requests

--- restdude-client-ember/app/routes/spaces/space/members.js
+++ restdude-client-ember/app/routes/spaces/space/members.js
@@ -5,4 +5,13 @@ export default Ember.Route.extend({
   model: function() {
     return this.store.findAll('context-membership');
   }
+  /*queryParams: {
+     space_id: {
+       refreshModel: true
+     }
+   },
+
+  model(params) {
+    return this.get('store').findRecord('context-membership', params.space_id);
+}*/
 });
