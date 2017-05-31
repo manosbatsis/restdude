b3a5bc769fa443f61943d92216db7aa7c9142976
delta750 <nkapetanas@hotmail.com>
2017-05-31 22:38:38 +0300

    Revert "updated websites, apps template route"
    This reverts commit 69131acb52dab01c06b7f33b57b2ad90e73eadf2.

--- restdude-client-ember/app/models/website.js
+++ restdude-client-ember/app/models/website.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
-import Space from "./space";
 
-export default Space.extend({
+export default DS.Model.extend({
   name: DS.attr('string'),
   title: DS.attr('string')
 });
--- restdude-client-ember/app/routes/spaces/space/apps.js
+++ restdude-client-ember/app/routes/spaces/space/apps.js
@@ -1,8 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    var spaceId = this.paramsFor('spaces.space').space_id;
-    return this.store.query('context-membership', {'context': spaceId});
-  }
 });
