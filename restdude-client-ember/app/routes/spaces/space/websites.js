766c7bb6ea6b50c77b305d13d073d937ceeb9466
manos <manos@sobol>
2017-06-01 03:13:40 +0300

    fixed routes

--- restdude-client-ember/app/routes/spaces/space/websites.js
+++ restdude-client-ember/app/routes/spaces/space/websites.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    var spaceId = this.paramsFor('spaces.space').space_id;
+    return this.store.query('website', {'parent': spaceId});
+  }
 });
